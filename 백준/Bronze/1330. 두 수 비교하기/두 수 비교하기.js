81911ec98c6ea43ff69e8ed9bb1c516cde70be29
Lee Kyeong Joon <165972574+KyeongJooni@users.noreply.github.com>
2025-09-25 00:23:26 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim().split(" ").map(Number);
+
+const A = input[0];
+const B = input[1];
+
+if (A > B) {
+  console.log(">");
+} else if (A < B) {
+  console.log("<");
+} else {
+  console.log("==");
+}
