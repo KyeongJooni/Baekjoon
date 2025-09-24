ae0a117e88a39d76967aebd229ebb43488d6d192
Lee Kyeong Joon <165972574+KyeongJooni@users.noreply.github.com>
2025-09-25 00:27:22 +0900

    [Bronze V] Title: 윤년, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim();
+const year = Number(input);
+
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+  console.log(1);
+} else {
+  console.log(0);
+}
