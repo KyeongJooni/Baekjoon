605269dfa9adab68e392a294133147df36b7d2c9
Lee Kyeong Joon <165972574+KyeongJooni@users.noreply.github.com>
2025-09-26 21:37:25 +0900

    [Bronze V] Title: A+B - 8, Time: 88 ms, Memory: 9388 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").toString().trim().split("\n");
+
+const T = Number(input[0]);
+let result = [];
+
+for (let i = 1; i <= T; i++) {
+  const [A, B] = input[i].split(" ").map(Number);
+  result.push(`Case #${i}: ${A} + ${B} = ${A + B}`);
+}
+
+console.log(result.join("\n"));
