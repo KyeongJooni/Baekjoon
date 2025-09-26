6475169e176bb8e69c99fc198dca11cd1f4a6f3b
Lee Kyeong Joon <165972574+KyeongJooni@users.noreply.github.com>
2025-09-26 21:57:20 +0900

    [Bronze V] Title: A+B - 5, Time: 96 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").toString().trim().split("\n");
+
+let result = [];
+
+for (let i = 0; i < input.length; i++) {
+  const [A, B] = input[i].split(" ").map(Number);
+  if (A === 0 && B === 0) break;
+  result.push(A + B);
+}
+
+console.log(result.join("\n"));
