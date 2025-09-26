a6196a4a602db2e4597e21cbdac90fc8a6633590
Lee Kyeong Joon <165972574+KyeongJooni@users.noreply.github.com>
2025-09-26 21:18:30 +0900

    [Bronze V] Title: A+B - 3, Time: 100 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").toString().trim().split("\n");
+
+const T = Number(input[0]);
+
+for (let i = 1; i <= T; i++) {
+  const [A, B] = input[i].split(" ").map(Number);
+  console.log(A + B);
+}
