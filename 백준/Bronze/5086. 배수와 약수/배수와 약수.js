d8bcc36a67465f0cb4b985a332b95cbcf86cfd4e
Lee Kyeong Joon <165972574+KyeongJooni@users.noreply.github.com>
2025-10-04 22:25:25 +0900

    [Bronze III] Title: 배수와 약수, Time: 92 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  if (a === 0 && b === 0) break;
+
+  if (b % a === 0) {
+    console.log("factor");
+  } else if (a % b === 0) {
+    console.log("multiple");
+  } else {
+    console.log("neither");
+  }
+}
