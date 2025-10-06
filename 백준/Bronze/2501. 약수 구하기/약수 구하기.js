c85087d6781fcca8b14a1199ec2ac880f2d04e87
Lee Kyeong Joon <165972574+KyeongJooni@users.noreply.github.com>
2025-10-06 18:07:52 +0900

    [Bronze III] Title: 약수 구하기, Time: 108 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim().split(' ').map(Number);
+const [N, K] = input;
+
+const divisors = [];
+for (let i = 1; i <= N; i++) {
+  if (N % i === 0) divisors.push(i);
+}
+
+console.log(divisors[K - 1] || 0);
