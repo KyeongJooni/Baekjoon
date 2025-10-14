251a757bc0d58f081c669a8ed2c0e06852fe8a60
Lee Kyeong Joon <165972574+KyeongJooni@users.noreply.github.com>
2025-10-14 10:07:53 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim().split("\n").map(Number);
+
+const sum = input.reduce((a, b) => a + b, 0);
+
+if (sum !== 180) {
+  console.log("Error");
+} else if (input[0] === 60 && input[1] === 60 && input[2] === 60) {
+  console.log("Equilateral");
+} else if (input[0] === input[1] || input[1] === input[2] || input[0] === input[2]) {
+  console.log("Isosceles");
+} else {
+  console.log("Scalene");
+}
